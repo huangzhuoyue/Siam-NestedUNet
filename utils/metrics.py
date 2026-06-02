ff88e91d5899b6b519a2295e38160c33f05054fc
HZY <63896554+huangzhuoyue@users.noreply.github.com>
2026-06-03 01:36:17 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -63,7 +63,7 @@ def dice_loss(logits, true, eps=1e-7):
     """
     num_classes = logits.shape[1]
     if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = torch.eye(num_classes + 1, device=true.device)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         true_1_hot_f = true_1_hot[:, 0:1, :, :]
         true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -72,7 +72,7 @@ def dice_loss(logits, true, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = torch.eye(num_classes, device=true.device)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(logits, dim=1)
     true_1_hot = true_1_hot.type(logits.type())
@@ -98,7 +98,7 @@ def jaccard_loss(logits, true, eps=1e-7):
     """
     num_classes = logits.shape[1]
     if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = torch.eye(num_classes + 1, device=true.device)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         true_1_hot_f = true_1_hot[:, 0:1, :, :]
         true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -107,7 +107,7 @@ def jaccard_loss(logits, true, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = torch.eye(num_classes, device=true.device)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(logits, dim=1)
     true_1_hot = true_1_hot.type(logits.type())
@@ -147,7 +147,7 @@ class TverskyLoss(nn.Module):
         """
         num_classes = logits.shape[1]
         if num_classes == 1:
-            true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+            true_1_hot = torch.eye(num_classes + 1, device=true.device)[true.squeeze(1)]
             true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
             true_1_hot_f = true_1_hot[:, 0:1, :, :]
             true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -156,7 +156,7 @@ class TverskyLoss(nn.Module):
             neg_prob = 1 - pos_prob
             probas = torch.cat([pos_prob, neg_prob], dim=1)
         else:
-            true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+            true_1_hot = torch.eye(num_classes, device=true.device)[true.squeeze(1)]
             true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
             probas = F.softmax(logits, dim=1)
 
